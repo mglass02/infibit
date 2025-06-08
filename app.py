f47735d31940712326cd70eb470ffaa82395ac47
micha <michael.andrew.glass@gmail.com>
2025-06-08 20:56:28 +0100

    j

--- app.py
+++ app.py
@@ -36,9 +36,6 @@ def get_db_connection():
         conn.close()
 
 def init_db():
-    if os.path.exists("infibit.db"):
-        os.remove("infibit.db")
-        logger.info("Deleted infibit.db to recreate with new schema.")
     try:
         with get_db_connection() as conn:
             cursor = conn.cursor()
